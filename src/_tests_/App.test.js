fb76c44e09d63233a0d2d320277b2cee2ccf72bd
Sirvelasque <velasquezwalter10@yahoo.es>
2022-07-28 13:17:17 -0600

    Create tests
    - snapshot testings for every component
    - interaction testing

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,31 +1,106 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import mockAxios from 'axios';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import  store  from '../redux/ConfigureStore';
-import { DETAIL } from '../redux/Companies/Companies'
-import { ONEDETAILS } from '../redux/details/details';
-import Categories from '../components/Categories/Categories';
+import App from '../App'
+import { act } from 'react-test-renderer';
 
-import animes from '../_mocks_/animes';
 
+jest.mock('axios');
 const CategorieMuck = () => {
-  const animeArray = store.getState().animes;
-  const anime = store.getState().details;
-  const categorie = store.getState().categorie;
-
   render(
     <Provider store={store}>
-      <Categories />
+      <App />
     </Provider>
   );
 };
 
-describe('test', () => {
-  store.dispatch({ type: 'CATEGORIECHANGE', payload: 'Action'});
-  test('check', () => {
-    CategorieMuck();
-    
-    expect(1).toBe(1);
-  })
-})
+describe('Render and interaction', () => {
+  afterEach(cleanup);
+  test('render testing', async() => {
+    mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({data: {
+      data: {
+        mal_id: 1,
+        title: 'Cowboy Bebop',
+        rank: 37,
+        year: 1998,
+        score: 8,
+        scored_by: 800,
+        popularity: 42,
+        genres: [
+          {
+            name: 'Drama',
+          },
+        ],
+        images:{
+          webp:{
+            image_url:'',
+          },
+          jpg:{
+            large_image_url:'',
+          },
+        },
+
+
+      },
+    },
+  }),
+    );
+    await act(() =>{
+      CategorieMuck();
+    });
+    expect(screen.getByText(/Drama/i)).toBeInTheDocument();
+    expect(screen.getByText(/Action/i)).toBeInTheDocument();
+    expect(screen.getByText(/Gourmet/i)).toBeInTheDocument();
+    expect(screen.getByText(/Slice of Life/i)).toBeInTheDocument();
+    expect(screen.getByText(/Fantasy/i)).toBeInTheDocument();
+  });
+
+  test('Change category from default to drama', async() => {
+    mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({data: {
+      data: {
+        mal_id: 1,
+        title: 'Cowboy Bebop',
+        rank: 37,
+        year: 1998,
+        score: 8,
+        scored_by: 800,
+        popularity: 42,
+        genres: [
+          {
+            name: 'Drama',
+          },
+        ],
+        images:{
+          webp:{
+            image_url:'',
+          },
+          jpg:{
+            large_image_url:'',
+          },
+        },
+
+
+      },
+    },
+  }),
+    );
+    await act(() =>{
+      CategorieMuck();
+    });
+    const dramaBtn = screen.getByText(/Drama/i);
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act (()=>{
+      fireEvent.click(dramaBtn);
+    });
+
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
+
+    const categorie = store.getState().categorie;
+    expect(categorie).toBe('Drama');
+  });
+});
--- src/_tests_/Details.test.js
+++ src/_tests_/Details.test.js
@@ -2,14 +2,57 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 
 import Details from '../components/Details';
+import Menu from '../components/Menu';
+import Navbar from '../components/NavBar';
+import Categories from '../components/Categories/Categories';
+import App from '../App'
 
-const Container = () => {
-  <BrowserRouter>
-    <Details />
-  </BrowserRouter>;
-};
+test('render details', () => {
+  const Container = () => {
+    <BrowserRouter>
+      <Details />
+    </BrowserRouter>;
+  };
+  const tree = renderer.create(<Container />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-it('renders correctly', () => {
+test('renders Menu', () => {
+  const Container = () => {
+    <BrowserRouter>
+      <Menu />
+    </BrowserRouter>;
+  };
+  const tree = renderer.create(<Container />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Render Categories', () => {
+  const Container = () => {
+    <BrowserRouter>
+      <Categories />
+    </BrowserRouter>;
+  };
+  const tree = renderer.create(<Container />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Render Navbar', () => {
+  const Container = () => {
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>;
+  };
+  const tree = renderer.create(<Container />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Render App', () => {
+  const Container = () => {
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>;
+  };
   const tree = renderer.create(<Container />).toJSON();
   expect(tree).toMatchSnapshot();
 });
