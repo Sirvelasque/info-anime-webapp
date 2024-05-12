52909ca8190bf42cb4e8ba3cb3c620873bbd4608
Walter Velasquez <velasquezwalter10@yahoo.es>
2024-05-11 18:22:40 -0600

    handle first state with local storate and errors

--- src/redux/Categories/Categories.js
+++ src/redux/Categories/Categories.js
@@ -1,8 +1,33 @@
 const CATEGORIECHANGE = 'CATEGORIECHANGE';
 
-const categorieReducer = (state = 'Action', action) => {
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('category');
+    if (serializedState === null) {
+      return 'Action'; // Return your default state if nothing is in localStorage
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return 'Action'; // Return default state if any errors
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('category', serializedState);
+  } catch (err) {
+    // Log errors or handle as you see fit
+    console.log('Could not save state', err);
+  }
+};
+const initialState = loadState(); // Load the state from localStorage
+
+const categorieReducer = (state = initialState, action) => {
   switch (action.type) {
-    case CATEGORIECHANGE:
+    case 'CATEGORIECHANGE':
+      // Immediately save the new state to localStorage whenever it changes
+      saveState(action.payload);
       return action.payload;
     default:
       return state;
